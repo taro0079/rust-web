d898177769953d5c0e15d2e8e30b267ae7c9177a
Taro Morita <morita0079@gmail.com>
2023-12-13 02:56:30 +0900

    first commit

---
+++ src/main.rs
@@ -0,0 +1,36 @@
+// use askama::Template;
+use axum::{response::Html, routing::get, Router};
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::registry()
+        .with(
+            tracing_subscriber::EnvFilter::try_from_default_env()
+                .unwrap_or_else(|_| "rust-web=debug,tower_http=debug".into()),
+        )
+        .with(tracing_subscriber::fmt::layer())
+        .init();
+
+    let app = Router::new().route("/", get(handler));
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
+        .await
+        .unwrap();
+    tracing::debug!("Listening on {}", listener.local_addr().unwrap());
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn handler() -> Html<&'static str> {
+    Html("<h1>Hello, world!</h1>")
+}
+
+// #[derive(Template)]
+// #[template(path = "home.html")]
+// struct Home {
+//     tweets: Vec<TweetView>,
+// }
+//
+// struct TweetView {
+//     name: String,
+//     message: String,
+//     posted_at: String,
+// }
